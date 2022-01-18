581eb3e3f005846194b7b6798b74e5c77457f451
mosalman <mosalman1098@gmail.com>
2022-01-18 12:01:41 +0530

    clock and user logo imported to recipecard component

--- src/Assets/Logos/index.js
+++
@@ -1,4 +0,0 @@
-import {ReactComponent as Time} from "./clock.png";
-import {ReactComponent as User} from "./users.png";
-
-export {Time,User};
--- src/Components/Header/Recipecards/Recipecard.js
+++ src/Components/Header/Recipecards/Recipecard.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./Recipecard.css"
-import {Time,User} from "../../../Assets/Logos/index"
+import  Clock from "../../../Assets/Logos/clock.png";
+import User from "../../../Assets/Logos/users.png";
 
 export default function Recipecard() {
     return (
         <div className='recipecard'>
             <div className='imagebox'></div>
             <span className='title'>Title</span>
-            <div></div>
+            <div>
+                <img src={Clock} alt="Clocklogo"/>
+                <img src={User} alt="Userlogo" />
+            </div>
         </div>
     )
 }
