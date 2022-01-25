008175e6be7ee41498c433b0bccfe770a6857571
mosalman <mosalman1098@gmail.com>
2022-01-25 23:22:15 +0530

    login page design completed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,15 +3,20 @@ import "./Login.css"
 
 
 let Login = ()=>{
+    const[username,setusername] = useState("")
+    const[password,setPassword] = useState("")
+    
     return(
         <div className="Container">
-            <h1>Login</h1>
+            <h1>Food App</h1>
             <form>
                <div className="text_field">
-                  <input type="text" placeholder="Username"  value="" />
+                  <input type="text" onClick={(e)=>setusername(e.target.value)} value={username} />
+                  <label>Username</label>
                </div>
                <div className="text_field">
-                  <input type="password" placeholder="Password"  value="" />
+                  <input type="password"  onClick={(e)=>setPassword(e.target.value)} value={password} />
+                  <label>Password</label>
                </div>
                <input className="button" type="button"  value="Sign In" />
             </form>
